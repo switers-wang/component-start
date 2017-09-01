1867d9d93403297c5c79ade1f1fa1c1709819e01
王叙鹏 <wangxupeng@wangxupengdeMacBook-Pro.local>
2017-09-01 14:05:07 +0800

    .

---
+++ src/Demo/entries/demo.js
@@ -0,0 +1,10 @@
+/**
+ * Created by wangxupeng on 2017/8/31.
+ */
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+    <div>{'hello world'}</div>,
+    document.getElementById('app'),
+);
--- src/actions/demo.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by wangxupeng on 2017/5/4.
- */
