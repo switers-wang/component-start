960375fa446ae0d61efc3a712d16e56c03a6984c
王叙鹏 <wangxupeng@wangxupengdeMacBook-Pro.local>
2017-09-01 15:11:11 +0800

    .

--- scripts/gt.js
+++ scripts/gt.js
@@ -1,3 +1,10 @@
 /**
  * Created by wangxupeng on 2017/9/1.
  */
+
+// using `babel-register`
+if (!global._babelPolyfill) {
+    require('babel-polyfill');
+}
+require('babel-register');
+module.exports = require('./gt/index');
---
+++ scripts/gt/index.js
@@ -0,0 +1,35 @@
+/**
+ * Created by wangxupeng on 2017/9/1.
+ */
+
+import Listr from 'listr';
+import execa from 'execa';
+
+const copyFiles = (options) => {
+    const { presets } = options;
+    const files = [
+        'docs',
+        'mock-server',
+        'scripts/config.js',
+        'scripts/dev.js',
+        'src',
+        '.babelrc',
+        '.editorconfig',
+        '.gitignore',
+        '.npmrc',
+        'LICENSE',
+    ];
+    return async() => {
+        await sleep(500);
+        await presets.copyFiles(files);
+    };
+};
+
+export const init = (options) => {
+    return new Listr([
+        {
+            title: 'copy files',
+            task: copyFiles(options),
+        },
+    ]);
+};
